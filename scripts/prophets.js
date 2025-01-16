74a13303c7a85bf89e94b59d2172ad5492931bc5
josephperalta03011993 <joseph.peralta03011993@gmail.com>
2025-01-16 17:36:09 +0800

    update create cards dynamically using js

--- scripts/prophets.js
+++ scripts/prophets.js
@@ -4,8 +4,37 @@ const cards = document.querySelector("#cards");
 async function getProphetData() {
     const response = await fetch(url);
     const data = await response.json();
+    
     // for testing the data response
-    console.table(data);
+    // console.table(data);
+
+    // this function expects an array that is why we will use data.prophets instead of just data
+    displayProphets(data.prophets);
+}
+
+const displayProphets = (prophets) => {
+    // build cards
+    prophets.forEach(prophet => {
+        // Create elements to add to the div.cards element
+        let card = document.createElement('section');
+        let fullName = document.createElement('h2');
+        let portrait = document.createElement('img');
+
+        // Build h2 show prophet fullname
+        fullName.textContent = `${prophet.name} ${prophet.lastname}`;
+        // Build img portrait set attributes
+        portrait.setAttribute('src', prophet.imageurl);
+        portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}`);
+        portrait.setAttribute('loading', 'lazy');
+        portrait.setAttribute('width', '340');
+        portrait.setAttribute('height', '440');
+
+        // Append the section(card) with the created elements
+        card.appendChild(fullName);
+        card.appendChild(portrait);
+
+        cards.appendChild(card);
+    });
 }
 
 getProphetData();
