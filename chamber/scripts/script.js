b3fa1121cf8edb9566ee66cdcbba4b0cc5b96118
josephperalta03011993 <joseph.peralta03011993@gmail.com>
2025-01-18 02:38:35 +0800

    footer content added dynamic dates

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -96,3 +96,11 @@ list.addEventListener('click', () => {
 });
 
 /* End of Members directory script */
+
+// Footer
+const lastModified = document.querySelector('#lastModified');
+const currentYear = document.querySelector('#currentyear');
+
+// Footer updates
+currentYear.textContent = new Date().getFullYear();
+lastModified.textContent = document.lastModified;
