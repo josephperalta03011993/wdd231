2947d6678724154bfa5c24a811275229f28a4a30
josephperalta03011993 <joseph.peralta03011993@gmail.com>
2025-01-25 13:35:42 +0800

    update working forecast for 3 days

--- chamber/scripts/weather-api.js
+++ chamber/scripts/weather-api.js
@@ -6,18 +6,21 @@ const humidity = document.querySelector('#humidity');
 const sunrise = document.querySelector('#sunrise');
 const sunset = document.querySelector('#sunset');
 const weatherIcon = document.querySelector('#img-weather');
+const today = document.querySelector('#today');
+const tomorrow = document.querySelector('#tomorrow');
+const dayThree = document.querySelector('#day-three');
 
 const APIKey = "3242e7f2673b3b98a4a563d053ce258a";
 const lat = "14.815761836194232";
 const lon = "121.07264465914291";
 const apiURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIKey}&units=metric`;
+const apiURLForecast = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIKey}&units=metric`;
 
 async function apiFetch() {
     try {
       const response = await fetch(apiURL);
       if (response.ok) {
         const data = await response.json();
-        console.log(data); // testing only
         displayResults(data);
       } else {
           throw Error(await response.text());
@@ -26,8 +29,24 @@ async function apiFetch() {
         console.log(error);
     }
 }
-  
+
+async function apiForecastFetch() {
+    try {
+      const response = await fetch(apiURLForecast);
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data); // testing only
+        displayForecastResults(data);
+      } else {
+          throw Error(await response.text());
+      }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
 apiFetch();
+apiForecastFetch();
 
 function displayResults(data) {
     const desc = data.weather[0]['description'].toUpperCase();
@@ -50,4 +69,32 @@ function displayResults(data) {
     weatherIcon.setAttribute('WIDTH', 150);
     weatherIcon.setAttribute('HEIGHT', 150);
     weatherIcon.setAttribute('OBJECT-FIT', 'contain');
+}
+
+function displayForecastResults(data) {
+    
+  try {
+    const temp = Math.round(data['list'][0].main.temp);
+    const tempTomorrow = Math.round(data['list'][6].main.temp);
+    const tempNextDay = Math.round(data['list'][14].main.temp);
+
+    const getDateDay = (dateString) => {
+      const date = new Date(dateString);
+      return date.toLocaleDateString('en-PH', { weekday: 'long' });
+    }
+
+    const tempDate = data['list'][0].dt_txt;
+    const tempTomorrowDate = data['list'][6].dt_txt;
+    const thirdDaytDate = data['list'][14].dt_txt;
+
+    const todayText = getDateDay(tempDate);  
+    const tomorrowText = getDateDay(tempTomorrowDate);
+    const thirdDayDate = getDateDay(thirdDaytDate);
+
+    today.innerHTML = `${todayText}: ${temp}&deg; C`;
+    tomorrow.innerHTML = `${tomorrowText}: ${tempTomorrow}&deg; C`;
+    dayThree.innerHTML = `${thirdDayDate}: ${tempNextDay}&deg; C`;
+  } catch (error) {
+    console.log(error);
+  }
 }
