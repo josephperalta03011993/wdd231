c58969c3fc3cad113ef63b8b81360acb1b7bf60c
josephperalta03011993 <joseph.peralta03011993@gmail.com>
2025-01-31 23:06:14 +0800

    UI for mobile view

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -1,1 +1,15 @@
-alert('test');
+const hamburger = document.querySelector("#hamburger");
+const nav = document.querySelector(".navigation");
+
+hamburger.addEventListener('click', () => {
+    nav.classList.toggle('open');
+    hamburger.classList.toggle('open');
+});
+
+// Footer
+const lastModified = document.querySelector('#lastModified');
+const currentYear = document.querySelector('#currentyear');
+
+// Footer updates
+currentYear.textContent = new Date().getFullYear();
+lastModified.textContent = document.lastModified;
