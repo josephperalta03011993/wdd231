8cba2408b47fcd31c916c93ef3d7df87e6b80384
josephperalta03011993 <joseph.peralta03011993@gmail.com>
2025-02-01 00:55:40 +0800

    update add animation for divs for membership cards

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -13,3 +13,21 @@ const currentYear = document.querySelector('#currentyear');
 // Footer updates
 currentYear.textContent = new Date().getFullYear();
 lastModified.textContent = document.lastModified;
+
+window.addEventListener('load', () => {
+    const membershipLevels = document.querySelectorAll('#membership-levels div');
+    membershipLevels.forEach((membershipLevels, index) => {
+        setTimeout(() => {
+            membershipLevels.classList.add('show');
+        }, index * 600); // Adjust delay between animations here
+    });
+
+    // const bronze = document.querySelector('#bronze');
+    // const silver = document.querySelector('#silver');
+    // const gold = document.querySelector('#gold');
+    
+    
+    // bronze.classList.add('show');
+    // silver.classList.add('show');
+    // gold.classList.add('show');
+});
