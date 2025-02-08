4554de1a8f2f0bb273fbbe54b61152a98cf2c9ca
josephperalta03011993 <joseph.peralta03011993@gmail.com>
2025-02-09 00:09:51 +0800

    add js for burger menu

--- chamber/scripts/discover-script.js
+++ chamber/scripts/discover-script.js
@@ -1,3 +1,19 @@
+const hamburger = document.querySelector("#hamburger");
+const nav = document.querySelector(".navigation");
+
+hamburger.addEventListener('click', () => {
+    nav.classList.toggle('open');
+    hamburger.classList.toggle('open');
+});
+
+// Footer
+const lastModified = document.querySelector('#lastModified');
+const currentYear = document.querySelector('#currentyear');
+
+// Footer updates
+currentYear.textContent = new Date().getFullYear();
+lastModified.textContent = document.lastModified;
+
 // name, address, and description 
 // Build 8 cards using an h2 for the title, 
 // a figure tag for the image, 
